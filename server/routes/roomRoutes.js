8e4889c7964140adb5e9697684c5fed7c8410759
Shivee Gupta <guptashivee23@gmail.com>
2026-05-21 17:51:18 +0530

    fix variable

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,19 +1,16 @@
 import express from "express";
-import upload from "../middleware/uploadMiddleware.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { 
+import {
   createRoom,
   getAllRooms,
-  getOwnerRooms, 
-  toggleRoomAvailability 
+  getOwnerRooms,
+  toggleRoomAvailability
 } from "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
-// BUG FIX: Public GET route add kiya - home page ke liye saare rooms
 roomRouter.get('/', getAllRooms);
-
-roomRouter.post('/', protect, upload.array("images", 4), createRoom);
+roomRouter.post('/', protect, createRoom); // ✅ upload middleware hata diya
 roomRouter.get('/owner-rooms', protect, getOwnerRooms);
 roomRouter.post('/toggle-availability', protect, toggleRoomAvailability);
 
