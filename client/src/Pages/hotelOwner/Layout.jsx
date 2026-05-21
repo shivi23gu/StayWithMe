08bee60dda22d815e78911427225a2dd30d3707f
Shivee Gupta <guptashivee23@gmail.com>
2026-05-21 20:52:46 +0530

    fix

--- client/src/Pages/hotelOwner/Layout.jsx
+++ client/src/Pages/hotelOwner/Layout.jsx
@@ -6,30 +6,30 @@ import { useAppContext } from '../../context/AppContext'
 
 const Layout = () => {
 
-const {isOwner, navigate} = useAppContext();
+  const { isOwner, navigate } = useAppContext();
 
-useEffect(()=>{
-    if(!isOwner){
-        navigate('/')
+  useEffect(() => {
+    if (!isOwner) {
+      navigate('/')
     }
-},[isOwner])
+  }, [isOwner])
 
-  return (
-    // min-h-screen ensures the background/layout always fills the window
-    <div className='min-h-screen flex flex-col bg-gray-50'>
-      <Navbar />
-      
-      {/* flex-1 here tells this container to take all space below the Navbar */}
-      <div className='flex flex-1'>
-        <Sidebar />
-        
-        {/* Added overflow-y-auto so your dashboard content is scrollable */}
-        <main className='flex-1 p-4 pt-8 md:px-10 overflow-y-auto'>
-           <Outlet />
-        </main>
-      </div>
-    </div>
-  )
+  return (
+    // min-h-screen ensures the background/layout always fills the window
+    <div className='min-h-screen flex flex-col bg-gray-50'>
+      <Navbar />
+      
+      {/* flex-1 here tells this container to take all space below the Navbar */}
+      <div className='flex flex-1'>
+        <Sidebar />
+        
+        {/* Added overflow-y-auto so your dashboard content is scrollable */}
+        <main className='flex-1 p-4 pt-8 md:px-10 overflow-y-auto'>
+           <Outlet />
+        </main>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
